f2627f409abf1c9208f1e8add09d52103dffd61c
makiintoch <makiintoch@gmail.com>
2018-08-24 17:00:21 +0300

    add Reports component

---
+++ src/components/Reports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Report.css';
+
+const Report = props => {
+    return (
+        <div className={'reports'}>
+            Reports
+        </div>
+    );
+}
+
+export default Report;
