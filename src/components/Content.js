715b7ad43f06eeee91dd5779f2057a0e6904bc5a
makiintoch <makiintoch@gmail.com>
2018-08-24 17:47:26 +0300

    edit route and add link

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './Content.css';
+import TimeTracker from './TimeTracker';
+import Reports from './Reports';
 
 const Content = props => {
     return (
         <div className={'content'}>
-            content
+            <Switch>
+                <Route exact path={'/'} component={TimeTracker}/>
+                <Route path={'/reports'} component={Reports}/>
+            </Switch>
         </div>
     );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
 
 const Sidebar = props => {
     return (
         <div className={'sidebar'}>
             <ul>
-                <li></li>
+                <li><NavLink to={'/'} activeClassName={'active'}>Time Tracker</NavLink></li>
+                <li><NavLink to={'/reports'} activeClassName={'active'}>Reports</NavLink></li>
             </ul>
         </div>
     );
