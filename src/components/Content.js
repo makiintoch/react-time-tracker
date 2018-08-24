d59a5d630cb25040c4e724fa95bf7b72ffd5cf29
makiintoch <makiintoch@gmail.com>
2018-08-24 14:34:44 +0300

    add Sidebar and Content components

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css';
+
+const Content = props => {
+    return (
+        <div className={'content'}>
+            content
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = props => {
+    return (
+        <div className={'sidebar'}>
+            sidebar
+        </div>
+    );
+}
+
+export default Sidebar;
